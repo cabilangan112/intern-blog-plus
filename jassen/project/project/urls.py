48cef4ce4561dabfbf2f52688c3ca5f8867ff682
Cabilangan112 <jassencabilangan@gmail.com>
2018-04-22 02:46:05 +0800

    login fix

--- jassen/project/project/urls.py
+++ jassen/project/project/urls.py
@@ -18,11 +18,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from user.views import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
-    url(r'^accounts/',include('django.contrib.auth.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
+    url(r'^register/$', RegisterView.as_view(), name='register'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- jassen/project/user/views.py
+++ jassen/project/user/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import RegisterForm
 
 # Create your views here.
+class RegisterView(CreateView):
+	form_class = RegisterForm
+	template_name = 'registration/register.html'
+	success_url = '/'
+
+	def dispatch(self, *args, **kwargs):
+		if self.request.user.is_authenticated():
+			return redirect("/logout")
+		return super(RegisterView, self).dispatch(*args, **kwargs)
