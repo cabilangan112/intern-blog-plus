231f5b831d826a778400a2bba790578033e25c6a
Cabilangan112 <jassencabilangan@gmail.com>
2018-04-21 23:03:54 +0800

    Blog models

--- jassen/project/blog/models.py
+++ jassen/project/blog/models.py
@@ -8,3 +8,46 @@ POST_STATUS = (
     ('draft', 'Draft'),
     ('hidden', 'Hidden'),
 )
+class Blog(models.Model):
+    heading = models.CharField(max_length=150)
+    sub_heading = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return '{}'.format(self.heading)
+
+class Post(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    sub_Title = models.CharField(max_length=150)
+    banner_photo = models.ImageField(upload_to = 'static/media')
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE) 
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    tags = models.ManyToManyField("Tag",related_name="tags")
+    status = models.CharField(max_length=9, choices=POST_STATUS, blank=True, default=True)
+    Comment = models.ManyToManyField("comment",related_name="Comment")
+    
+    def __str__(self):
+        return '{}'.format(self.user)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    vote = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}'.format(self.user)
+
+class Category(models.Model):
+    title = models.CharField(max_length=150)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=150)
+
+    def __str__(self):
+        return '{}'.format(self.title)
