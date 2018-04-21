df14e152b6252a6e76f2104bba491f74b1fdba98
Cabilangan112 <jassencabilangan@gmail.com>
2018-04-22 03:15:30 +0800

    Register View

--- jassen/project/user/views.py
+++ jassen/project/user/views.py
@@ -3,14 +3,13 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import RegisterForm
-
+from django.views.generic import (ListView,DetailView,UpdateView,CreateView)
 # Create your views here.
+
 class RegisterView(CreateView):
 	form_class = RegisterForm
 	template_name = 'registration/register.html'
 	success_url = '/'
 
 	def dispatch(self, *args, **kwargs):
-		if self.request.user.is_authenticated():
-			return redirect("/logout")
 		return super(RegisterView, self).dispatch(*args, **kwargs)
